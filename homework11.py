7f780584db3b9b41e119f4622d70f7e5b4d7097e
gating654 <168896707+gating654@users.noreply.github.com>
2024-06-13 22:59:06 +0700

    Add files via upload

---
+++ homework11.py
@@ -0,0 +1,11 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        numberOfFloors = floors
+        return print(numberOfFloors)
+
+
+h1 = House()
+h1.setNewNumberOfFloors(3)
